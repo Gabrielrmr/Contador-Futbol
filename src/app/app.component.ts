34181a47914b2b1dc693958cf59ca13f907066e2
murallas <murallas512bv@gmail.com>
2024-03-15 18:59:19 -0500

    C2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContadorPuntosComponent } from './contador-puntos/contador-puntos.component';
-import { ContadorTiempoComponent } from './contador-tiempo/contador-tiempo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ContadorPuntosComponent,ContadorTiempoComponent],
+  imports: [RouterOutlet,ContadorPuntosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/contador-puntos/contador-puntos.component.ts
+++ src/app/contador-puntos/contador-puntos.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgModel } from '@angular/forms';
+import { ContadorTiempoComponent } from '../contador-tiempo/contador-tiempo.component';
 
 @Component({
   selector: 'app-contador-puntos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule,ContadorTiempoComponent],
   templateUrl: './contador-puntos.component.html',
   styleUrl: './contador-puntos.component.css'
 })
 export class ContadorPuntosComponent {
+
+  showComponent = false;
+
+  toggleComponent() {
+    this.showComponent = !this.showComponent;
+  }
+  Time=0;
+  timeOptions = [1, 2, 5];
+  selectedTime: number = 1;
+  Timepersonalizado=0;
+  timeLeft: number = 60;
+  interval: any;
+
+  EnviarTiempo(){
+    if(this.selectedTime==1){
+      this.Time=1
+    }
+    else if(this.selectedTime==2){
+      this.Time=2
+    }
+    else if(this.selectedTime==5){
+      this.Time=5
+    }
+    else{
+      this.Time=this.Timepersonalizado
+    }
+  }
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      if (this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        this.timeLeft = 60;
+      }
+    }, 1000);
+    }
+
+    IniciarTemporizador(){
+
+      if (this.Time > 0) {
+        this.timeLeft = this.Time;
+        this.startTimer();
+
+    }
+    }
 
 }
